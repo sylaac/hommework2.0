0c6b2633424b2c0f056f1d2dd19c7dfd5a19a1f8
sylaac <59823446+sylaac@users.noreply.github.com>
2020-01-25 15:01:39 +0100

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,16 +1,21 @@
-name=input('what is your name ?')
+name = input('what is your name? ')
 print("hello", name)
 choice = "yes"
 while "yes" == choice:
- a=input('give me the real number you want to devise')
- b=input('give me the divisor')
- a=float(a)
- b=float(b)
- c=a%b
- if c < b/2 :
-     d=int(a/b)
- else :
-     d=int(a/b+1)
- print("the rounded division is",d)
- choice=input("tape yes if you want to do a new division")
-
+    a = input('give me the real number you want to devise: ')
+    b = input('give me the divisor')
+    a = float(a)
+    b = float(b)
+    if b == 0:
+        print("sorry you can't divide by 0")
+    else :
+        c = abs(a) % abs(b)
+        if c < abs(b) / 2:
+            d = int(abs(a) / abs(b))
+        else :
+            d = int(abs(a) / abs(b) + 1)
+        if a < 0 or b < 0:
+            if a > 0 or b > 0:
+                d = -d
+        print("the rounded division is", d)
+    choice = input("tape yes if you want to do a new division")
